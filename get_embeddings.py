3da9871a83ef923f6830d9ef96614dd08265a5b6
dthuremella <divyat@alumni.princeton.edu>
2024-12-12 10:56:33 +0000

    script for embeddings

---
+++ get_embeddings.py
@@ -0,0 +1,166 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from pathlib import Path
+import argparse
+import json
+import math
+import os
+import sys
+import time
+import pickle
+
+import torch
+import torch.nn.functional as F
+from torch import nn, optim
+import torch.distributed as dist
+import torchvision.datasets as datasets
+
+import augmentations as aug
+from distributed import init_distributed_mode
+from main_vicreg import adjust_learning_rate, exclude_bias_and_norm
+from main_vicreg import VICReg, LARS, Projector
+
+import resnet
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Pretrain a resnet model with VICReg", add_help=False)
+
+    # Data
+    parser.add_argument("--data-dir", type=Path, default="../unitraj/media/shared/nuScenes", required=False,
+                        help='Path to the image net dataset')
+
+    # Checkpoints
+    parser.add_argument("--exp-dir", type=Path, default="./exp",
+                        help='Path to the experiment folder, where all logs/checkpoints will be stored')
+    parser.add_argument("--log-freq-time", type=int, default=60,
+                        help='Print logs to the stats.txt file every [log-freq-time] seconds')
+
+    # Model
+    parser.add_argument("--arch", type=str, default="resnet50",
+                        help='Architecture of the backbone encoder network')
+    parser.add_argument("--mlp", default="8192-8192-8192",
+                        help='Size and number of layers of the MLP expander head')
+
+    # Optim
+    parser.add_argument("--epochs", type=int, default=100,
+                        help='Number of epochs')
+    parser.add_argument("--batch-size", type=int, default=2048,
+                        help='Effective batch size (per worker batch size is [batch-size] / world-size)')
+    parser.add_argument("--base-lr", type=float, default=0.2,
+                        help='Base learning rate, effective learning after warmup is [base-lr] * [batch-size] / 256')
+    parser.add_argument("--wd", type=float, default=1e-6,
+                        help='Weight decay')
+
+    # Loss
+    parser.add_argument("--sim-coeff", type=float, default=25.0,
+                        help='Invariance regularization loss coefficient')
+    parser.add_argument("--std-coeff", type=float, default=25.0,
+                        help='Variance regularization loss coefficient')
+    parser.add_argument("--cov-coeff", type=float, default=1.0,
+                        help='Covariance regularization loss coefficient')
+
+    # Running
+    parser.add_argument("--num-workers", type=int, default=10)
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+
+    # Distributed
+    parser.add_argument('--world-size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--rank', default=0, type=int)
+    parser.add_argument('--dist-url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+
+def main(args):
+    torch.backends.cudnn.benchmark = True
+    init_distributed_mode(args)
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    dist.init_process_group("gloo", rank=args.rank, world_size=args.world_size)
+    print(args)
+    gpu = torch.device(args.device)
+
+    if args.rank == 0:
+        args.exp_dir.mkdir(parents=True, exist_ok=True)
+        stats_file = open(args.exp_dir / "stats.txt", "a", buffering=1)
+        print(" ".join(sys.argv))
+        print(" ".join(sys.argv), file=stats_file)
+
+    transforms = aug.TrainTransform()
+
+    dataset = datasets.ImageFolder(args.data_dir / "train", transforms) 
+    sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
+    assert args.batch_size % args.world_size == 0
+    per_device_batch_size = args.batch_size // args.world_size
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=per_device_batch_size,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        sampler=sampler,
+    )
+
+    model = VICRegEmb(args).cuda(gpu)
+    model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    optimizer = LARS(
+        model.parameters(),
+        lr=0,
+        weight_decay=args.wd,
+        weight_decay_filter=exclude_bias_and_norm,
+        lars_adaptation_filter=exclude_bias_and_norm,
+    )
+
+    if (args.exp_dir / "model.pth").is_file():
+        if args.rank == 0:
+            print("resuming from checkpoint")
+        ckpt = torch.load(args.exp_dir / "model.pth", map_location="cpu")
+        start_epoch = ckpt["epoch"]
+        model.load_state_dict(ckpt["model"])
+        optimizer.load_state_dict(ckpt["optimizer"])
+    else:
+        start_epoch = 0
+
+    start_time = last_logging = time.time()
+    scaler = torch.cuda.amp.GradScaler()
+    epoch = start_epoch
+    print('epoch ', epoch)
+    sampler.set_epoch(epoch)
+    x_embeddings = []
+    y_embeddings = []
+    for step, ((x, y), _) in enumerate(loader, start=epoch * len(loader)):
+        x = x.cuda(gpu, non_blocking=True)
+        y = y.cuda(gpu, non_blocking=True)
+
+        lr = adjust_learning_rate(args, optimizer, loader, step)
+
+        optimizer.zero_grad()
+        with torch.cuda.amp.autocast():
+            x_emb, y_emb = model.get_embeddings(x, y)
+            x_embeddings.append(x_emb)
+            y_embeddings.append(y_emb)
+    embeddings = {'x': x_embeddings, 'y': y_embeddings}
+    with open("vicreg_embeddings.pkl", "wb") as f:
+        pickle.dump(embeddings, f)
+
+class VICRegEmb(VICReg):
+  
+    def get_embeddings(self, x, y):
+        x_emb = self.projector(self.backbone(x))
+        y_emb = self.projector(self.backbone(y))
+        return x_emb, y_emb
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('VICReg training script', parents=[get_arguments()])
+    args = parser.parse_args()
+    main(args)
