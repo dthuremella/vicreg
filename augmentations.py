455e02aba9ddb763d4ab4e41e437c0cc6b28a97d
dthuremella <divyat@alumni.princeton.edu>
2024-12-13 06:20:19 +0000

    remove transforms that might not do well for maps

--- augmentations.py
+++ augmentations.py
@@ -38,21 +38,21 @@ class TrainTransform(object):
     def __init__(self):
         self.transform = transforms.Compose(
             [
-                transforms.RandomResizedCrop(
+                transforms.Resize(
                     224, interpolation=InterpolationMode.BICUBIC
                 ),
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomApply(
-                    [
-                        transforms.ColorJitter(
-                            brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
-                        )
-                    ],
-                    p=0.8,
-                ),
-                transforms.RandomGrayscale(p=0.2),
-                GaussianBlur(p=1.0),
-                Solarization(p=0.0),
+                # transforms.RandomHorizontalFlip(p=0.5),
+                # transforms.RandomApply(
+                #     [
+                #         transforms.ColorJitter(
+                #             brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
+                #         )
+                #     ],
+                #     p=0.8,
+                # ),
+                # transforms.RandomGrayscale(p=0.2),
+                # GaussianBlur(p=1.0),
+                # Solarization(p=0.0),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
@@ -65,17 +65,17 @@ class TrainTransform(object):
                     224, interpolation=InterpolationMode.BICUBIC
                 ),
                 transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomApply(
-                    [
-                        transforms.ColorJitter(
-                            brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
-                        )
-                    ],
-                    p=0.8,
-                ),
-                transforms.RandomGrayscale(p=0.2),
-                GaussianBlur(p=0.1),
-                Solarization(p=0.2),
+                # transforms.RandomApply(
+                #     [
+                #         transforms.ColorJitter(
+                #             brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
+                #         )
+                #     ],
+                #     p=0.8,
+                # ),
+                # transforms.RandomGrayscale(p=0.2),
+                # GaussianBlur(p=0.1),
+                # Solarization(p=0.2),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
